992a39535c52c665fc6c1df75f976f1a66d7d377
Callum Denby <callum.denby@arcadiagroup.co.uk>
2017-07-21 14:54:30 +0100

    Fixed insecure bug

--- imports/client/dogs.jsx
+++ imports/client/dogs.jsx
@@ -4,7 +4,7 @@ export default class Dogs extends Component {
   render() {
     return (
       <div className="item dogs" onClick={this.props.onClick}>
-        <div className="label counter">Dogs</div>
+        <div className="label counter">Turtles</div>
         <div className="label counter">{this.props.count}</div>
       </div>
     )
